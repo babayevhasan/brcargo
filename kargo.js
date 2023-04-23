3eefd296962aafcdfad3079a7fa53872c1c1f01d
Hasan Babayev <125999948+babayevhasan@users.noreply.github.com>
2023-04-23 14:47:07 +0400

    Add files via upload

---
+++ kargo.js
@@ -0,0 +1,38 @@
+
+// Part Of NavBar
+
+
+// const menuHamburger = document.querySelector(".burger")
+// const navLinks = document.querySelector(".navbar")
+
+// menuHamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('mobile-menu')
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+//Part Of About 
+
+// $(".learnmore button").click(function(){
+//     $(".all").hide();
+//     $(".aboutstatistics").hide();
+//     $(".learnmore button").hide();
+//     $(".learnmore2").show();
+//     $(".learnmore2 button").show();
+
+//   });
+//   $(".learnmore2 button").click(function(){
+//     $(".all").show();
+//     $(".aboutstatistics").show();
+//     $(".learnmore button").show();
+//     $(".learnmore2").hide();
+//   });
