f15fab9ceee1321d4d73bd99ac937ff0ba47c510
#MT <miri.tagiyev999@gmail.com>
2023-04-23 22:21:17 +0400

    ssss

--- kargo.js
+++ kargo.js
@@ -1,38 +1,10 @@
-
 // Part Of NavBar
 
+const menuHamburger = document.querySelector(".burger");
+const navLinks = document.querySelector(".navbar");
 
-// const menuHamburger = document.querySelector(".burger")
-// const navLinks = document.querySelector(".navbar")
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
 
-// menuHamburger.addEventListener('click', () => {
-//     navLinks.classList.toggle('mobile-menu')
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-//Part Of About 
-
-// $(".learnmore button").click(function(){
-//     $(".all").hide();
-//     $(".aboutstatistics").hide();
-//     $(".learnmore button").hide();
-//     $(".learnmore2").show();
-//     $(".learnmore2 button").show();
-
-//   });
-//   $(".learnmore2 button").click(function(){
-//     $(".all").show();
-//     $(".aboutstatistics").show();
-//     $(".learnmore button").show();
-//     $(".learnmore2").hide();
-//   });
+//Part Of About
